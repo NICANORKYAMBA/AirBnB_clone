7b232ec03bcb51d506142126340c3fa747204dc4
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-02-09 15:08:54 +0300

    Task 5

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """The base class of the Airbnb"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -27,23 +28,22 @@ class BaseModel:
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' and key == 'updated_at':
+                if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != '__class__':
+                if key != "__class__":
                     setattr(self, key, value)
-                if 'created_at' not in kwargs:
-                    self.created_at = BaseModel.created_at
                 if 'id' not in kwargs:
-                    self.id = BaseModel.id
+                    self.id = str(uuid.uuid4())
+                if 'created_at' not in kwargs:
+                    self.created_at = datetime.now()
 
                 if 'created_at' in kwargs and 'updated_at' not in kwargs:
-                    self.updated_at = BaseModel.created_at
+                    self.updated_at = self.created_at
                 else:
-                    self.updated_at = BaseModel.updated_at
+                    self.updated_at = datetime.now()
         else:
-            self.id = BaseModel.id
-            self.created_at = BaseModel.created_at
-            self.updated_at = BaseModel.updated_at
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string of class name, id, and dictionary
@@ -56,15 +56,17 @@ class BaseModel:
         'updated_at' with current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all key/
         values of '__dict__' of the instance
         """
         my_dict = dict(self.__dict__)
-        my_dict['__class__'] = str(type(self).__name__)
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['id'] = self.id
-        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["id"] = self.id
+        my_dict["updated_at"] = self.updated_at.isoformat()
 
         return my_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""file storage class of Airbnb"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary '__objects'"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in '__objects' the obj with key
+        '<obj class name>.id'"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        """Serializes '__objects' to the JSON file
+        (path:__file_path)"""
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
+            json.dump(my_dict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to '__objects'
+        (only if the JSON file (__file_path) exists;
+        otherwise, do nothing.)"""
+        try:
+            with open(self.__file_path, 'r', encoding="utf-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value['__class__'])(**value)
+                    self.__objects[key] = value
+        except FileNotFoundError:
+            pass
+    
+    def close(self):
+        """Call reload method"""
+        self.reload()
