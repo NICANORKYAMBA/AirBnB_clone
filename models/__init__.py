ce6712745211ae1ca6aff6e6383c9b2b381ecfa2
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-02-12 09:54:55 +0300

    eliminating code errors

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance"""
 
-# from models.base_model import BaseModel
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
